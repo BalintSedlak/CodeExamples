b025123abfc3abbd2768bfcb5eeb7062cba4db64
Balint Sedlak <balint.sedlak@gmail.com>
2021-02-16 00:21:08 +0100

    example project for implicit and explicit keyword

---
+++ C#/ImplicitAndExplicitKeyword/ImplicitAndExplicitKeyword/ExplicitKeywordExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Apartment
+{
+    public string Name { get; set; }
+    public static explicit operator House(Apartment a)
+    {
+        return new House() { Name = a.Name };
+    }
+}
+
+class House
+{
+    public string Name { get; set; }
+    public static explicit operator Apartment(House h)
+    {
+        return new Apartment() { Name = h.Name };
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        House h = new House();
+        h.Name = "Broadway";
+
+        // Cast a House to an Apartment.
+        Apartment a = (Apartment)h;
+
+        // Apartment was converted from House.
+        Console.WriteLine(a.Name);
+    }
+}
---
+++ C#/ImplicitAndExplicitKeyword/ImplicitAndExplicitKeyword/ImplicitKeywordExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ImplicitKeywordExample
+{
+    class Machine
+    {
+        public int _value;
+        public static implicit operator Widget(Machine m)
+        {
+            Widget w = new Widget();
+            w._value = m._value * 2;
+            return w;
+        }
+    }
+
+    class Widget
+    {
+        public int _value;
+        public static implicit operator Machine(Widget w)
+        {
+            Machine m = new Machine();
+            m._value = w._value / 2;
+            return m;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Machine m = new Machine();
+            m._value = 5;
+            Console.WriteLine(m._value);
+
+            // Implicit conversion from machine to widget.
+            Widget w = m;
+            Console.WriteLine(w._value);
+
+            // Implicit conversion from widget to machine.
+            Machine m2 = w;
+            Console.WriteLine(m2._value);
+        }
+    }
+}
